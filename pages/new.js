482cf75ac6e9e1fcfd3aff7faf7b03c1bb545f93
Madhu5A3 <madhuv299@gmail.com>
2020-06-13 17:13:00 +0530

    Update: New Note Component Updated

--- pages/new.js
+++ pages/new.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import fetch from 'isomorphic-unfetch';
 import { Button, Form, Loader } from 'semantic-ui-react';
 import { useRouter } from 'next/router';
+import style from '../components/css/style.module.css';
 
 const New = () => {
     const [formState, setformState] = useState({ title: '', description: '' })
@@ -10,30 +11,88 @@ const New = () => {
     const [errors, setErrors] = useState({});
     const router = useRouter();
 
-    return (
-        <div className="form-container">
-            <h1>Create Note</h1>
-            <div> {
-                isSubmitting ? <Loader active inline='centered' />
-                    : <Form onSubmit={handleSubmit}>
-                        <Form.Input 
-                            fluid
-                            label='Title'
-                            placeholder='Title'
-                            name='title'
-                            onChange={handleChange}
-                        />
-                        <Form.TextArea 
-                            fluid
-                            label='Description'
-                            placeholder= 'Description'
-                            name='Description'
-                            onChange={handleChange}
-                        />
+    useEffect(() => {
+        if (isSubmitting) {
+            if (Object.keys(errors).length === 0) {
+                createNote(); //post request
+                // alert('Success');
+            }
+            else {
+                setIsSubmitting(false);
+            }
+        }
+    }, [errors]);
 
-                        <Button type='Submit'>Create</Button>
-                    </Form>
-            }</div>
+    const createNote = async () => {
+        try {
+            const res = await fetch('http://localhost:3000/api/notes', {
+                method: 'POST',
+                headers : {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(formState)
+            })
+
+            router.push('/');
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let errs = validate();
+        setErrors(errs);
+        setIsSubmitting(true)
+    };
+    const handleChange = (e) => {
+        setformState({
+            ...formState,
+            [e.target.name]: e.target.value
+        })
+    };
+
+    const validate = () => {
+        let err = {};
+
+        if (!formState.title) {
+            err.title = 'Title is Required'
+        }
+
+        if (!formState.description) {
+            err.description = 'Description is Required'
+        }
+
+        return err;
+    };
+    return (
+        <div className={style.formcontainer}>
+            <h1>Create Note</h1>
+            <div>
+                {
+                    isSubmitting ? <Loader active inline='centered' />
+                        : <Form onSubmit={handleSubmit}>
+                            <Form.Input
+                                fluid
+                                error={errors.title ? { content: 'Please enter a title', pointing: 'below' } : null}
+                                label='Title'
+                                placeholder='Title'
+                                name='title'
+                                onChange={handleChange}
+                            />
+                            <Form.TextArea
+                                fluid
+                                error={errors.description ? { content: 'Please enter a description', pointing: 'below' } : null}
+                                label='Description'
+                                placeholder='Description'
+                                name='description'
+                                onChange={handleChange}
+                            />
+
+                            <Button type='submit'>Create</Button>
+                        </Form>
+                }
+            </div>
         </div>
 
     );
